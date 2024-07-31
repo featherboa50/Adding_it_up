66eeb0a8ce6000d71d09f549cc3d16404670c207
featherboa50 <featherboa50@yahoo.com>
2024-07-31 14:27:47 -0700

    changes

--- hospitalized.R
+++ hospitalized.R
@@ -13,6 +13,10 @@ if(!require(kamila)) install.packages("kamila", repos = "https://cran.rstudio.co
 if(!require(caret)) install.packages("caret", repos = "https://cran.rstudio.com/")
 if(!require(recipes)) install.packages("recipes", repos = "https://cran.rstudio.com/")
 if(!require(MLmetrics)) install.packages("MLmetrics", repos = "https://cran.rstudio.com/")
+if(!require(GGally)) install.packages("GGally", repos = "https://cran.rstudio.com/")
+if(!require(ggbiplot)) install.packages("ggbiplot", repos = "https://cran.rstudio.com/")
+if(!require(pls)) install.packages("pls", repos = "https://cran.rstudio.com/")
+
 
 
 #library(readr)
@@ -22,6 +26,9 @@ library(kamila)
 library(caret)
 library(recipes)
 library(MLmetrics) #LogLoss/Cross-Entropy Loss
+library(GGally)
+library(ggbiplot)
+library(pls)
 
 
 #color blind friendly colors
@@ -40,8 +47,10 @@ cb_cols <-
 
 
 getHdata(support2)
-#remove fields that are findings from previous models 
-support <- data.frame(support2) %>% select(-aps, -sps, -surv2m, -surv6m, -prg2m, -prg6m, -dnr, -dnrday)
+#remove fields that are findings from previous models and those deceased before discharge 
+support <- data.frame(support2) %>% 
+  select(-aps, -sps, -surv2m, -surv6m, -prg2m, -prg6m, -dnr, -dnrday) %>%
+  filter(hospdead == 0)
 
 # Final hold-out test set will be 10% of patient's data
 set.seed(365, sample.kind="Rounding") # if using R 3.6 or later
@@ -71,11 +80,24 @@ main <- clear.labels(main)
 
 summary(main)
 
-main %>% group_by(sex) %>% summarise(n())
+# checking which columns have NA and how many
+x <- colSums(is.na(main))
+# less than 25% missing
+x[which(x < (nrow(main)/4) & x > 0)]
+# more than 25% missing values
+x[which(x >= (nrow(main)/4))]
+# no missing values
+x[which(x == 0)]
 
+#effect of age, higher spread 
+main %>%  ggplot(aes(age, fill = (death == 1))) + geom_histogram(binwidth = 10)
+
+
+#effect of sex
+main %>% group_by(sex) %>% summarise(n())
 main %>% group_by(sex, death) %>% ggplot(aes(death == 1,age)) + geom_boxplot(aes(color = sex))
 
-#exploring race as
+#exploring race as a factor
 main %>% group_by(race, death) %>% summarise(age = mean(age)) %>% arrange(desc(death))
 main %>% group_by(race, death) %>% summarise(age = mean(age)) %>% 
   ggplot(aes(race,age, fill = death == 1)) + 
@@ -96,18 +118,21 @@ main %>% ggplot(aes(num.co,  fill = death == 1)) +
   geom_histogram(stat="count", position="dodge") 
 
 
+# Possible causes columns based on too many missing values or is a dependent var
+modeling_index = sort(c(c(1,5,9,10,12,18,seq(22,31),34,35), c(3,7,17,19,20,21)))
+# adding column 2 as predictor, adjust the indicies to view different comparisons
+# visual correlation matrix
+# Can take several minutes to run, my system would fail on over 12 at a time. 
+# 1-10 seemed to have the best results
+reference <- main[,c(2,modeling_index[1:10])]
+ggpairs(reference, aes(color = as.factor(death), alpha = 0.4))
+
+
 #------------ Cleaning up NAs
 
-# checking which columns have NA and how many
-x <- colSums(is.na(main))
-# less than 25% missing
-x[which(x < 2048 & x > 0)]
 
-# more than 25% missing values
-x[which(x >= 2048)]
-x[which(x == 0)]
 
-# Baseline Normal Fill-in Values
+# Baseline Normal Fill-in Values (recommened by source)
 # Serum albumin	3.5
 # PaO2/FiO2 ratio (pafi)	333.3
 # Bilirubin	1.01
@@ -126,20 +151,19 @@ main <- main %>% replace_na(list(alb = 3.5, pafi = 333.3,
                                  edu = 12))
 
 
+
+
+
 #use mean for NA, numeric
-main <- main %>% mutate_at(vars("meanbp", "hrt", "resp", "scoma", "sod", "temp"),
-                                   ~ifelse(is.na(.x), mean(.x, na.rm = TRUE), .x))
-#---  redo here
+main <- main %>% mutate_at(vars("charges", "totcst", "avtisst"),
+                           ~ifelse(is.na(.x), mean(.x, na.rm = TRUE), .x))
+#mode for catagorical
+main <- main %>% mutate_at(vars("sfdm2"),
+                           ~ifelse(is.na(.x), mode(.x), .x))
 
-#mode for NA, categorical data
-support2 <- support2 %>% mutate_at(vars("ca"),
-                                   ~ifelse(is.na(.x), mode(.x), .x))
+
 # check correlation in numeric data
-sort(abs(cor(support2 %>% select(where(is.numeric)), use = "complete.obs"))[,2], decreasing=TRUE)
-
-# support2 %>% filter(death == 1) %>% pairs(~sex + bun  + sod + race, data = .)
-# charges totcst avtisst race  temp sfdm2 
-
+sort(abs(cor(main %>% select(where(is.numeric)), use = "complete.obs"))[,2], decreasing=TRUE)
 
 
 
@@ -150,11 +174,7 @@ test_index <- createDataPartition(y = main$death, times = 1, p = 0.2, list = FAL
 train <- main[-test_index,]
 test <- main[test_index,]
 
-# Possible causes
-modeling_index = c(c(1,5,9,10,12,18,seq(22,31),34,35), c(3,7,17,19,20,21))
-# columns not used for predictions mostly due to unhandeled NAs, used for recipes
-# -1 due death being removed from the indexes when creating recipes
-id_index = sapply(c(4,6,8,11,seq(13,16),32,33,seq(36,39)),function(x){x-1}) 
+
 
 
 # just using mean
@@ -163,21 +183,125 @@ RMSE(mu,test$death)
 # Calculate BCE
 LogLoss(mu, test$death)
 
-#Worse than mean
+#Linear Regression
 relevant_data <- train[,c(2,modeling_index)]
 fit <- lm(death ~ ., data = relevant_data)
 RMSE(predict(fit,test),test$death)
 LogLoss(predict(fit,test), test$death)
 
-#TRY WITH EACH INDIVIDUALLY
-#THEN COMBINE WITH LOWEST X
+# graident boosted method
+# significantly worse
+if(!require(gbm)) install.packages("gbm", repos = "https://cran.rstudio.com/")
+library(gbm)
 
-#REGULARIZE?
+lambdas <- seq(2, 10)
+trees <- sapply(lambdas, function(lambda){
+  fit <- gbm(death ~ ., data = relevant_data, cv.folds = 2, 
+             n.trees = lambda,
+             distribution = "bernoulli")
+  LogLoss(predict(fit,test),test$death)
+})
+qplot(lambdas, trees, geom = "line")
+lambda_u <- lambdas[which.min(trees)]
+print(lambda_u)
 
-#PCA
-#TREE
+
+fit <- gbm(death ~ ., data = relevant_data, cv.folds = 5, n.trees = lambda_u)
+RMSE(predict(fit,test),test$death)
+LogLoss(predict(fit,test), test$death)
 
 
+
+#Exploring PCA----------------------------------------------------------------
+
+#explaining PCA https://www.turing.com/kb/guide-to-principal-component-analysis
+
+#calculate principal components
+PCA_data <- relevant_data %>% mutate(death_info = paste(row_number(),death))
+row.names(PCA_data) =   PCA_data$death_info
+# remove categorical data
+results <- prcomp(PCA_data[,-c(3,5,9,13,26)], scale = TRUE)
+summary(results)
+
+#scree plot
+var_explained <- results$sdev^2/sum(results$sdev^2)
+qplot(c(1:21), var_explained) + 
+  geom_line() + 
+  xlab("Principal Component") + 
+  ylab("Variance Explained") +
+  ggtitle("Scree Plot") +
+  ylim(0, 0.15)
+
+# top results in relation to death
+sort(abs(results$rotation[1,]), decreasing = TRUE)
+
+plotting_results <- results$x %>% as.data.frame %>% rownames_to_column("death_info") %>%
+  separate("death_info",c("row","death")) 
+
+# overall plot with the components that explain overall variance
+plotting_results %>%
+  ggplot(aes(PC1, PC2)) + geom_point(aes(color = death, alpha = 0.2))
+# top 2 components that relate to death instead
+plotting_results %>%
+  ggplot(aes(PC11, PC9)) + geom_point(aes(color = death, alpha = 0.2))
+
+# ordering features based on importance for overall top and death top
+y<-data.frame(results$rotation) %>% mutate(feature = row.names(.))
+y %>% ggplot(aes(PC1, reorder(feature, PC1))) + geom_point()
+y %>% ggplot(aes(PC11, reorder(feature, PC11))) + geom_point()
+
+
+#biplot with top 2 
+ggbiplot(results, choices = c(11,9), obs.scale = 1, var.scale = 1, 
+         groups = as.factor(relevant_data$death)) + 
+  scale_colour_manual(values = c("0" = cb_cols[1], "1" = cb_cols[2])) +
+  theme(legend.direction = 'horizontal', legend.position = 'top') + 
+  ylim(-6, 6)
+
+
+
+y %>% ggplot(aes(PC11, PC9, label = feature)) +
+  geom_text()
+
+
+model <- lm(response ~ results$x[, 1] + results$x[, 2])
+summary(model)
+predict(results,test)
+
+#make this example reproducible
+set.seed(1)
+
+#fit PCR model
+model <- pcr(death~., data=relevant_data, scale=TRUE, validation="CV")
+summary(model)
+
+#visualize cross-validation plots
+validationplot(model)
+
+pcr_pred <- predict(model, test, ncomp=30)
+RMSE(pcr_pred,test$death)
+LogLoss(pcr_pred, test$death)
+#---------------------------------
+
+
+
+# Tree method
+# Improvement on binary but loss on RMSE
+# Min 5 observations per node
+# 10 cross validations
+library(rpart)
+fit <- rpart(death ~ ., data = select(train, -c(d.time, sfdm2, hospdead)), 
+             control = rpart.control( minsplit = 5, xval = 10))
+plot(fit, margin = 0.1)
+text(fit, cex = 0.75)
+RMSE(predict(fit,test),test$death)
+LogLoss(predict(fit,test), test$death)
+
+
+#----RECIPES
+
+# -1 due death being removed from the indexes when creating recipes
+id_index = sapply(c(4,6,8,11,seq(13,16),32,33,seq(36,39)),function(x){x-1}) 
 
 my_recipe <- recipe(death ~ ., data = train) %>%
   update_role(all_of(id_index), new_role = "ID")  #sets the title feature to be kept but not used for modeling
@@ -188,23 +312,9 @@ my_recipe
 print(summary(my_recipe),n = 40)
 #library(kernlab)
 
-# Support Vector Machine with Radial
-# runs for very long time
-tox_ctrl <- trainControl(method = "cv")
-set.seed(888)
-train_svm <- train(my_recipe, train,
-                   method = "svmRadial", 
-                   metric = "wRMSE",
-                   maximize = FALSE,
-                   tuneLength = 10,
-                   trControl = tox_ctrl)
-train_svm
-#RMSE is higher so example of overfitting
-RMSE(predict(train_svm, testing), testing$rating)
-
-
 
 #loess method
+# No improvement
 b <- 5
 control <- trainControl(method = "cv", number = b, p = .9)
 train_Loess <- train(my_recipe, train,
@@ -212,30 +322,20 @@ train_Loess <- train(my_recipe, train,
                      #tuneGrid = data.frame(k = seq(1,60,2)),
                      trControl = control)
 
-train_Loess
 RMSE(predict(train_Loess, test), test$death)
 LogLoss(predict(train_Loess, test), test$death)
 
-#loess method
-b <- 5
-control <- trainControl(method = "cv", number = b, p = .9)
-train_knn <- train(my_recipe, train,
-                     method = "knn",
-                     tuneGrid = data.frame(k = seq(1,60,2)),
-                     trControl = control)
+# #knn method
+# b <- 5
+# control <- trainControl(method = "cv", number = b, p = .9)
+# train_knn <- train(my_recipe, train,
+#                      method = "knn",
+#                      tuneGrid = data.frame(k = seq(1,60,2)),
+#                      trControl = control)
+# 
+# RMSE(predict(train_Loess, testing), testing$death)
+# LogLoss(predict(train_Loess, test), test$death)
 
-train_knn
-RMSE(predict(train_Loess, testing), testing$death)
-LogLoss(predict(train_Loess, test), test$death)
-
-train_ada <- train(my_recipe, train,
-                   method = "lssvmLinear",
-                   #tuneGrid = data.frame(k = seq(1,60,2)),
-                  )
-
-train_knn
-RMSE(predict(train_Loess, testing), testing$death)
-LogLoss(predict(train_Loess, test), test$death)
 
 
 #-----------------
@@ -280,25 +380,17 @@ LogLoss(predict(train_Loess, test), test$death)
  table(kamtest$death, kamPred)
  
  RMSE(kamPred,kamtest$death)
- binary_cross_entropy(kamtest$death,kamPred)
+ LogLoss(kamtest$death,kamPred)
  
+ # FIGURE OUT HOW TO DO THIS 
  # Plot KAMILA results
  plottingData <- cbind(
    conVars,
    catVarsFac,
-   KamilaCluster = factor(kamRes$finalMemb))
+   KamilaCluster = factor(kamRes$finalMemb),
+   death = factor(kamtrain$death))
  plottingData$result <- ifelse(
-   plottingData$death == '1', yes='Yes',no='No')
+   plottingData$KamilaCluster == '1', yes='Yes',no='No')
  
- ggplot(
-   plottingData,
-   aes(
-     x=logSpap,
-     y=Index.of.tumour.stage.and.histolic.grade,
-     color=ternarySurvival,
-     shape=KamilaCluster)) + geom_point()
+ plottingData %>% ggplot(aes(KamilaCluster, death)) + geom_histogram()
  
- # rm(kamtest, kamtest_dummy, kamtrain, kamtrain_dummy,
- #    testcatVarsFac, kamind, kamRes, catVarsFac, conTest, conVars)
- 
- # #-------------------
